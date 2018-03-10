617d7c1f1d957ea624ee7185f1bfa15348539a81
Ankur Lathwal <alathw2@uic.edu>
2018-03-10 03:36:44 -0600

    init: git, npm, express, bower,angularjs

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+  
+app.use(express.static(__dirname + '/../app'));
+ 
+const server = app.listen(process.env.PORT || 3000, function() {
+  const host = server.address().address;
+  const port = server.address().port;
+});
