38dc868f6f76af3655e87efcda7f1b289cc048b0
Ankur Lathwal <alathw2@uic.edu>
2018-03-12 02:05:41 -0500

    edit: controller endpoint for post

--- app/controllers/formController.js
+++ app/controllers/formController.js
@@ -6,7 +6,7 @@ app.controller('formController', function($scope,$http) {
     
     $scope.processForm = function() {
         console.log($scope.athlete);
-        $http.post("http://localhost:3000/athlete",$scope.athlete).then(function(response){
+        $http.post("/athlete",$scope.athlete).then(function(response){
             console.log("POST response :" + response.statusText);
         },function(error){
             console.log("An error occurred: " + error.statusText);             
